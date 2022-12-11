06b9552f0559409a0ad8401a6c83c03c8925a8a9
inty <interruptinuse@gmail.com>
2022-12-11 08:46:21 +0000

    Add CfgUnitInsignia >> ire_yorha

---
+++ addons/main/config.cpp
@@ -0,0 +1,32 @@
+class CfgMods {
+	class ire_yorha_insignia {
+		dir = "@ire_yorha_insignia";
+		name = "YoRHa Insignia";
+		picture = "A3\Ui_f\data\Logos\arma3_expansion_alpha_ca";
+		hidePicture = "true";
+		hideName = "true";
+	};
+};
+
+class CfgPatches {
+	class ire_yorha_insignia_main {
+		name = "YoRHa Insignia";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 2.02;
+		requiredAddons[] = {};
+		author = "tsar_bimbo";
+		version = 1.0;
+		versionStr = "1.0.0.0";
+		versionAr[] = {1, 0, 0, 0};
+	};
+};
+
+class CfgUnitInsignia {
+	class ire_yorha {
+		displayName = "YoRHa";
+		author = "tsar_bimbo";
+		texture = "\x\ire_yorha_insignia\yorha_ca.paa";
+		textureVehicle = "";
+	};
+};
